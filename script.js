b5a56eefaf295d36b63edc9310803f695ddfd63c
abraham-laha <haverkamp-lars@hotmail.com>
2023-01-08 14:57:54 +0100

    Let Grid display in html by js file

---
+++ script.js
@@ -0,0 +1,26 @@
+const divAußen = document.querySelector('.außen');
+
+
+function createGrid(columns){
+
+    for(let i=1; i<=columns;i++){
+        const newLine = document.createElement('div');
+        newLine.classList.add('zeile');
+        divAußen.appendChild(newLine);
+        console.log(i);
+    }
+    for(let i=0; i< columns; i++){
+        const lines = document.querySelectorAll('.zeile');
+        let line = lines[i];
+        for(let j=0;j<columns;j++){
+            const newBox = document.createElement('div');
+            newBox.classList.add('box');
+            line.appendChild(newBox);
+        }
+    }
+    console.log(`${divAußen.clientHeight}px`);
+    divAußen.style.width= `${divAußen.clientHeight}px`;
+}
+
+
+createGrid(5);
